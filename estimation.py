a25219d319f776a50ac2f7f15e3e4dd2db739ecd
birdalugureren <birdalugureren@gmail.com>
2020-12-01 14:30:28 +0300

    day problem

--- estimation.py
+++ estimation.py
@@ -35,16 +35,21 @@ log_mid = np.log(mid_price)
 log_mid = [(lambda x: log_mid.loc[:, x])(pair) for pair in itertools.combinations(log_mid.columns, 2)]
 
 # calculate pair diff
-log_mid = map(lambda x: (x.iloc[:, 0] - x.iloc[:, 1])._set_name('_'.join(x.columns)), log_mid)
+log_mid = list(map(lambda x: (x.iloc[:, 0] - x.iloc[:, 1])._set_name('_'.join(x.columns)), log_mid))
 
 # Remove NaN's
-log_mid = [(lambda x: x.dropna().values)(pair) for pair in log_mid]
+log_mid = [(lambda x: x.dropna())(pair) for pair in log_mid]
+
+# HH:MM:SS is being removed from the index
+log_mid = list(map(lambda x: x.droplevel(1), log_mid))
+
+# >>>>>> estimation functions >>>>>>>>>
 
 # Converting to r objects
 r_vectors = list(map(robjects.FloatVector, log_mid))
 
-# >>>>>> estimation functions >>>>>>>>>
 # parameters can be changed as specified in the documentation
+
 elw = [lmts.ELW(vector, **{'m': floor(1 + len(vector) ** 0.6), 'mean.est': "mean"})[0][0] for vector in r_vectors]
 
 # TODO: @taper parametresi 'Velasco' olarak atandığında hata alınır.
@@ -69,3 +74,31 @@ pair_names = ['_'.join(pair) for pair in itertools.combinations(mid_price.column
 estimation = pd.DataFrame(data={'elw': elw, 'elw2s': elw2s, 'gph': gph, 'hou_perron': hou_perron}, index=pair_names)
 
 estimation.to_csv('estimation.csv')
+
+
+def func(x):
+
+    vector = robjects.FloatVector(x)
+    elw = lmts.ELW(vector, **{'m': floor(1 + len(vector) ** 0.6), 'mean.est': "mean"})[0][0]
+
+    # TODO: @taper parametresi 'Velasco' olarak atandığında hata alınır.
+    elw2s = lmts.ELW2S(vector, **{'m': floor(1 + len(vector) ** 0.6), 'trend_order': 1, 'taper': 'HC'})[0][0]
+
+    gph = lmts.gph(vector, **{'m': floor(1 + len(vector) ** 0.6), 'l': 1})[0]
+
+    hou_perron = lmts.Hou_Perron(vector, **{'m': floor(1 + len(vector) ** 0.6)})[0][0]
+
+    # TODO: @taper parametresi 'Velasco' olarak atandığında hata alınır.
+    local_w = lmts.local_W(vector,
+                           **{'int': robjects.FloatVector([-0.5, 2.5]), 'm': floor(1 + len(vector) ** 0.6),
+                              'diff_param': 1,
+                              'taper': 'HC', 'l': 1})[0][0]
+    # estimation = pd.DataFrame(data={'elw': elw, 'elw2s': elw2s, 'gph': gph, 'hou_perron': hou_perron}, index=[x.name])
+    keys = ['elw', 'elw2s', 'gph', 'hou_perron']
+    date = [x.index[0], x.index[0], x.index[0], x.index[0]]
+    estimation = pd.DataFrame(data=[elw, elw2s, gph, hou_perron], columns=[x.name], index=[date, keys])
+
+    return estimation
+
+
+func(asd)
