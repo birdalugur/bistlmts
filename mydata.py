9301aeceba9db48326efd83d15a7af0f0b859dc2
birdalugureren <birdalugureren@gmail.com>
2020-12-01 11:10:19 +0300

    bist30

--- mydata.py
+++ mydata.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+BIST30 = ["ARCLK", "ASELS", "BIMAS", "DOHOL", "EKGYO", "FROTO", "HALKB", "GARAN", "ISCTR", "KCHOL", "KOZAA", "KOZAL",
+          "KRDMD", "AKBNK", "PETKM", "PGSUS", "SAHOL", "SISE", "EREGL", "SODA", "TAVHL", "TCELL", "THYAO", "TKFEN",
+          "TOASO", "TSKB", "TTKOM", "TUPRS", "VAKBN", "YKBNK"]
+
 parse_dates = ['date']
 
 
@@ -17,6 +21,16 @@ def download(symbols: list, start_date: str, end_date: str) -> pd.DataFrame:
     pass
 
 
+def is_bist30(x: str) -> bool:
+    """
+    Is the expression denoted by x a member of bist30?
+    """
+    for symbol in BIST30:
+        if symbol in x:
+            return True
+    return False
+
+
 def read(path: str) -> pd.DataFrame:
     """
     Bir klasörden csv verilerini okuyun.
@@ -31,14 +45,15 @@ def read(path: str) -> pd.DataFrame:
     all_paths = map(lambda x: path + x, listdir(path))
     all_data = []
     for _path in all_paths:
-        try:
-            all_data.append(
-                pd.read_csv(_path,
-                            dtype=dt,
-                            converters={'time': lambda x: pd.Timestamp(int(x))})
-            )
-        except pd.errors.EmptyDataError:
-            print("Empty Data:\n", _path)
+        if is_bist30(_path):
+            try:
+                all_data.append(
+                    pd.read_csv(_path,
+                                dtype=dt,
+                                converters={'time': lambda x: pd.Timestamp(int(x))})
+                )
+            except pd.errors.EmptyDataError:
+                print("Empty Data:\n", _path)
 
     return pd.concat(all_data)
 
