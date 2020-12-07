eaffb1172f334623293a3302decb235be8be3b3c
birdalugureren <birdalugureren@gmail.com>
2020-12-07 12:57:07 +0300

    null value error

--- lmts.py
+++ lmts.py
@@ -4,6 +4,7 @@ For documentation, use the docs() function.
 """
 
 from math import floor
+import pandas as pd
 
 from rpy2 import robjects
 from rpy2.robjects.packages import importr
@@ -29,38 +30,66 @@ def convert_Rvectors(values):
     return vector
 
 
+def isnull(values) -> bool:
+    """
+    Check empty data
+    """
+    if isinstance(values, pd.Series) and values.dropna().size == 0:
+        return True
+
+
 # >>>>>> estimation functions >>>>>>>>>
 
 
-def elw(values):
+def elw(values, mean_est="mean"):
     """
     parameters can be changed as specified in the documentation
     """
+    if isnull(values):
+        return None
     vector = convert_Rvectors(values)
-    _elw = lmts.ELW(vector, **{'m': floor(1 + len(vector) ** 0.6), 'mean.est': "mean"})[0][0]
+    _elw = lmts.ELW(vector, **{'m': floor(1 + len(vector) ** 0.6), 'mean.est': mean_est})[0][0]
     return _elw
 
 
-def elw2s(values):
+def elw2s(values, trend_order=1, taper='HC'):
+    """
+    Two-Step Exact local Whittle estimator of fractional integration with unknown mean and time trend.
+    """
     # TODO: @taper parametresi 'Velasco' olarak atandığında hata alınır.
+
+    if isnull(values):
+        return None
+
     vector = convert_Rvectors(values)
-    _elw2s = lmts.ELW2S(vector, **{'m': floor(1 + len(vector) ** 0.6), 'trend_order': 1, 'taper': 'HC'})[0][0]
+
+    try:
+        _elw2s = \
+            lmts.ELW2S(vector, **{'m': floor(1 + len(vector) ** 0.6), 'trend_order': trend_order, 'taper': taper})[0][0]
+    except:
+        raise Exception('Error for ', values.name)
     return _elw2s
 
 
-def gph(values):
+def gph(values, l_value=1):
+    if isnull(values):
+        return None
     vector = convert_Rvectors(values)
-    _gph = lmts.gph(vector, **{'m': floor(1 + len(vector) ** 0.6), 'l': 1})[0]
+    _gph = lmts.gph(vector, **{'m': floor(1 + len(vector) ** 0.6), 'l': l_value})[0]
     return _gph
 
 
 def hou_perron(values):
+    if isnull(values):
+        return None
     vector = convert_Rvectors(values)
     _hou_perron = lmts.Hou_Perron(vector, **{'m': floor(1 + len(vector) ** 0.6)})[0][0]
     return _hou_perron
 
 
 def local_w(values):
+    if isnull(values):
+        return None
     # TODO: @taper parametresi 'Velasco' olarak atandığında hata alınır.
     vector = convert_Rvectors(values)
     _local_w = lmts.local_W(vector,
